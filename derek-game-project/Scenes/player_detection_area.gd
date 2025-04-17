5748bb5c7b43c9e338f5b9992efc5da83afbfd31
BegliS <2916431s@student.gla.ac.uk>
2025-04-17 19:16:31 +0700

    text box added to npc, deleted unused script files, centered background in main scene

--- derek-game-project/Scenes/player_detection_area.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-
-
-func _on_Area2D_mouse_entered():
-	print("Welcome!")
--- derek-game-project/Scripts/npc.gd
+++ derek-game-project/Scripts/npc.gd
@@ -2,9 +2,10 @@ extends StaticBody2D
 
 
 func _on_Area2D_mouse_entered():
-	print("Welcome!")
 	$Sprite2D.modulate = Color.GREEN
+	$Label.visible = true 
 
 func _on_Area2D_mouse_exited():
-	print("Bye!")
+	
 	$Sprite2D.modulate = Color.RED
+	$Label.visible = false 
